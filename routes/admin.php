e6e4768694403a3c8c6711d916fee1a75f28d3e0
lalji_nivzen <lalji.gondaliya@nivzen.com>
2025-01-30 11:44:01 +0530

    remove unused Models

--- routes/admin.php
+++ routes/admin.php
@@ -59,41 +59,41 @@ Route::group(['middleware' => ['web', 'backend:backend']], function() {
 
 
     // Manage courses moddule
-    Route::get('courses', [CoursesController::class,'index'])->name('courses-manage');
-    Route::post('courses/list-ajax', [CoursesController::class,'anyListAjax'])->name('courses-list-ajax');
-    Route::any('courses/add', [CoursesController::class,'showCoursesForm'])->name('course-add-form');
-    Route::post('courses/add', [CoursesController::class,'addCourse'])->name('course-add');
-    Route::any('courses/edit/{id}', [CoursesController::class,'editCourse'])->name('course-edit');
-    Route::get('courses/view/{id}', [CoursesController::class,'show'])->name('course-view');
-    Route::get('courses/delete/{id}', [CoursesController::class,'deleteCourse'])->name('course-delete');
+//    Route::get('courses', [CoursesController::class,'index'])->name('courses-manage');
+//    Route::post('courses/list-ajax', [CoursesController::class,'anyListAjax'])->name('courses-list-ajax');
+//    Route::any('courses/add', [CoursesController::class,'showCoursesForm'])->name('course-add-form');
+//    Route::post('courses/add', [CoursesController::class,'addCourse'])->name('course-add');
+//    Route::any('courses/edit/{id}', [CoursesController::class,'editCourse'])->name('course-edit');
+//    Route::get('courses/view/{id}', [CoursesController::class,'show'])->name('course-view');
+//    Route::get('courses/delete/{id}', [CoursesController::class,'deleteCourse'])->name('course-delete');
 
 
     // Manage  modules
-    Route::get('modules', [ModulesController::class,'index'])->name('modules-manage');
-    Route::post('modules/list-ajax', [ModulesController::class,'anyListAjax'])->name('modules-list-ajax');
-    Route::any('modules/add', [ModulesController::class,'showModulesForm'])->name('module-add-form');
-    Route::post('modules/add', [ModulesController::class,'addModule'])->name('module-add');
-    Route::any('modules/edit/{id}', [ModulesController::class,'editModule'])->name('module-edit');
-    Route::get('modules/delete/{id}', [ModulesController::class,'deleteModule'])->name('module-delete');
+//    Route::get('modules', [ModulesController::class,'index'])->name('modules-manage');
+//    Route::post('modules/list-ajax', [ModulesController::class,'anyListAjax'])->name('modules-list-ajax');
+//    Route::any('modules/add', [ModulesController::class,'showModulesForm'])->name('module-add-form');
+//    Route::post('modules/add', [ModulesController::class,'addModule'])->name('module-add');
+//    Route::any('modules/edit/{id}', [ModulesController::class,'editModule'])->name('module-edit');
+//    Route::get('modules/delete/{id}', [ModulesController::class,'deleteModule'])->name('module-delete');
 
     // Manage batches modules
 
-    Route::get('batches', [BatchesController::class,'index'])->name('batches-manage');
-    Route::post('batches/list-ajax', [BatchesController::class,'anyListAjax'])->name('batches-list-ajax');
-    Route::any('batches/add', [BatchesController::class,'showBatcheForm'])->name('batche-add-form');
-    Route::post('batches/add', [BatchesController::class,'addBatche'])->name('batche-add');
-    Route::any('batches/edit/{id}', [BatchesController::class,'editBatche'])->name('batche-edit');
-    Route::any('batches/view/{id}', [BatchesController::class,'viewBatche'])->name('batche-view');
-    Route::get('batches/delete/{id}', [BatchesController::class,'deleteBatche'])->name('batche-delete');
+//    Route::get('batches', [BatchesController::class,'index'])->name('batches-manage');
+//    Route::post('batches/list-ajax', [BatchesController::class,'anyListAjax'])->name('batches-list-ajax');
+//    Route::any('batches/add', [BatchesController::class,'showBatcheForm'])->name('batche-add-form');
+//    Route::post('batches/add', [BatchesController::class,'addBatche'])->name('batche-add');
+//    Route::any('batches/edit/{id}', [BatchesController::class,'editBatche'])->name('batche-edit');
+//    Route::any('batches/view/{id}', [BatchesController::class,'viewBatche'])->name('batche-view');
+//    Route::get('batches/delete/{id}', [BatchesController::class,'deleteBatche'])->name('batche-delete');
 
 
-    Route::post('batches-content/list-ajax', [BatchesContentController::class,'anyListAjax'])->name('batches-content-list-ajax');
-    Route::any('batches-content/add/{id}', [BatchesContentController::class,'showBatcheContentForm'])->name('batche-content-add-form');
-    Route::post('batches-content/add/{id}', [BatchesContentController::class,'addBatcheContent'])->name('batche-content-add');
-    Route::any('batches-content/edit/{id}', [BatchesContentController::class,'editBatcheContent'])->name('batche-content-edit');
-    Route::any('batches-content/view/{id}', [BatchesContentController::class,'viewBatcheContent'])->name('batche-content-view');
-    Route::get('batches-content/delete/{id}', [BatchesContentController::class,'deleteBatcheContent'])->name('batche-content-delete');
-    Route::post('batches-content-upload', [BatchesContentController::class,'uploadLargeFiles'])->name('files.upload.large');
+//    Route::post('batches-content/list-ajax', [BatchesContentController::class,'anyListAjax'])->name('batches-content-list-ajax');
+//    Route::any('batches-content/add/{id}', [BatchesContentController::class,'showBatcheContentForm'])->name('batche-content-add-form');
+//    Route::post('batches-content/add/{id}', [BatchesContentController::class,'addBatcheContent'])->name('batche-content-add');
+//    Route::any('batches-content/edit/{id}', [BatchesContentController::class,'editBatcheContent'])->name('batche-content-edit');
+//    Route::any('batches-content/view/{id}', [BatchesContentController::class,'viewBatcheContent'])->name('batche-content-view');
+//    Route::get('batches-content/delete/{id}', [BatchesContentController::class,'deleteBatcheContent'])->name('batche-content-delete');
+//    Route::post('batches-content-upload', [BatchesContentController::class,'uploadLargeFiles'])->name('files.upload.large');
 
 
     // Manage Distributors modules
